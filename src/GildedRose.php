8bf9d69648961cfbbc79812207d12b0d7f3a6464
Krzysztof <mithrilmg@gmail.com>
2024-11-02 11:06:10 +0100

    Introduced more consts and replaced nested if statements with switch

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -6,55 +6,85 @@ namespace App;
 
 final class GildedRose
 {
-    public function updateQuality(Item $item)
+    private const MAX_NORMAL_QUALITY = 50;
+    private const MAX_LEGENDARY_QUALITY = 80;
+    private const SELL_BY_DATE_EXPIRE = 0;
+    private const SIX_DAYS = 6;
+    private const ELEVEN_DAYS = 11;
+
+    public function updateQuality(Item $item): void
     {
-        if ($item->name != Item::AGED_BRIE and $item->name != Item::BACKSTAGE_PASS) {
-            if ($item->quality > 0) {
-                if ($item->name != Item::SULFURAS_HAND_OF_RAGNAROS) {
-                    $item->quality = $item->quality - 1;
-                } else {
-                    $item->quality = 80;
-                }
-            }
-        } else {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-                if ($item->name == Item::BACKSTAGE_PASS) {
-                    if ($item->sell_in < 11) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                    if ($item->sell_in < 6) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
-            }
+        switch ($item->name) {
+            case Item::AGED_BRIE:
+                $this->updateAgedBrieQuality($item);
+                break;
+            case Item::BACKSTAGE_PASS:
+                $this->updateBackstagePassQuality($item);
+                break;
+            case Item::SULFURAS_HAND_OF_RAGNAROS:
+                $this->updateLegendaryQuality($item);
+                break;
+            default:
+                $this->updateDefaultQuality($item);
+                break;
+        }
+    }
+
+    private function updateAgedBrieQuality(Item $item): void
+    {
+        if ($item->quality < self::MAX_NORMAL_QUALITY) {
+            $item->quality = $item->quality + 1;
         }
 
-        if ($item->name != Item::SULFURAS_HAND_OF_RAGNAROS) {
-            $item->sell_in = $item->sell_in - 1;
-        }
+        $item->sell_in = $item->sell_in - 1;
 
         if ($item->sell_in < 0) {
-            if ($item->name != Item::AGED_BRIE) {
-                if ($item->name != Item::BACKSTAGE_PASS) {
-                    if ($item->quality > 0) {
-                        if ($item->name != Item::SULFURAS_HAND_OF_RAGNAROS) {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                } else {
-                    $item->quality = $item->quality - $item->quality;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+            if ($item->quality < self::MAX_NORMAL_QUALITY) {
+                $item->quality = $item->quality + 1;
             }
         }
     }
 
+    private function updateBackstagePassQuality(Item $item): void
+    {
+        if ($item->quality < self::MAX_NORMAL_QUALITY) {
+            $item->quality = $item->quality + 1;
+            if ($item->sell_in < self::ELEVEN_DAYS) {
+                if ($item->quality < self::MAX_NORMAL_QUALITY) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+            if ($item->sell_in < self::SIX_DAYS) {
+                if ($item->quality < self::MAX_NORMAL_QUALITY) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+        }
+
+        $item->sell_in = $item->sell_in - 1;
+
+        if ($item->sell_in < self::SELL_BY_DATE_EXPIRE) {
+            $item->quality = $item->quality - $item->quality;
+        }
+    }
+
+    private function updateLegendaryQuality(Item $item): void
+    {
+        $item->quality = self::MAX_LEGENDARY_QUALITY;
+    }
+
+    private function updateDefaultQuality(Item $item): void
+    {
+        if ($item->quality > 0) {
+            $item->quality = $item->quality - 1;
+        }
+
+        $item->sell_in = $item->sell_in - 1;
+
+        if ($item->sell_in < 0) {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
+        }
+    }
 }
